d9d2429e14729688c3afcc1720e916e7918c5cb4
Mason Karr <karr.mason@gmail.com>
2025-09-03 13:21:20 -0500

    layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -8,11 +8,9 @@ import Footer from './Footer'
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
     <div className="bg-gradient-to-br from from-gray-900 via-purple-900 to-indigo-900 ..." >
-      <header className='flex justify-between content-center'>                
-        < h1 className= "w-12 h-12 rounded-sm border-4 border-solid justify-center bg-radial from-indigo-900 to-purple-400"> 
-          Mason Karr
-        </h1>
-  
+      <header className='flex justify-between content-center'> 
+        <div className = "w-48"></div>               
+
          <Header></Header>
         <nav className=''>
           <Navbar ></Navbar>
@@ -21,6 +19,10 @@ const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
         <div></div>
           <main >{children}</main>
             <br />
+              <h1 className= "flex m-auto w-12 h-12 rounded-sm border-4 border-solid justify-center bg-radial from-indigo-900 to-purple-400"> 
+                Mason Karr
+              </h1>
+  
             <br />
               <Footer></Footer>  
     </div>
